5c1b5068f8586adc27bc3845de5f5b6e208641b5
Jorge04 <Jorge196@users.noreply.github.com>
2020-10-24 17:45:34 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,8 @@
 class SongsController < ApplicationController
+  before_filter :lookup_artist
+  before_filter :lookup_song
   def index
-    @songs = Song.all
+    @songs = @artist && @artist.songs || Song.all 
   end
 
   def show
@@ -45,6 +47,30 @@ class SongsController < ApplicationController
   end
 
   private
+
+  def lookup_artist
+    if params[:artist_id]
+      @artist = Artist.find_by(id: params[:artist_id])
+      if !@artist
+        flash[:alert] = "Artist not found"
+        redirect_to artists_path
+      end
+    end
+  end
+
+  def lookup_song
+    if params[:id]
+      if @artist
+        @song = @artist.songs.find_by(id: params[:id])
+        if !@song
+          flash[:alert] = "Song not found"
+          redirect_to(artist_songs_path(@artist))
+        end
+      else
+        @song = Song.find(params[:id])
+      end
+    end
+  end
 
   def song_params
     params.require(:song).permit(:title, :artist_name)
