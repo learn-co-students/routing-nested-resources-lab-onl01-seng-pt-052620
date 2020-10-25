8cca91d79156577e946f14d3e8de483ea859f4d2
bebofofum <bri.boehm@gmail.com>
2020-10-24 20:46:49 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,11 +2,11 @@ class SongsController < ApplicationController
 
   def index
     if params[:artist_id]
-      artist = Artist.find_by(id: params[:artist_id])
-      if artist
-       @songs = artist.songs 
+      @artist = Artist.find_by(id: params[:artist_id])
+      if @artist.nil?
+       redirect_to artists_path, alert: "Artist not found" 
       else
-        redirect_to artists_path
+        @songs = @artist.songs
       end
     else
       @songs = Song.all
@@ -14,19 +14,22 @@ class SongsController < ApplicationController
   end
 
   def show
-   binding.pry
-
-    @song = Song.find(params[:id])
+   if params[:artist_id]
     @artist = Artist.find_by(id: params[:artist_id])
-    if !@song
-      redirect_to artist_songs_path(@artist)
-    end
+    @song = @artist.songs.find_by(id: params[:id])
+      if @song.nil?
+        redirect_to artist_songs_path(@artist), alert: "Song not found" 
+      end
+    else
+      @song = Song.find(params[:id])
+   end
   end
 
   def new
-    @song = Song.new
-  end
+      @song = Song.new
+  end   
 
+    
   def create
     @song = Song.new(song_params)
 
@@ -39,6 +42,17 @@ class SongsController < ApplicationController
 
   def edit
     @song = Song.find(params[:id])
+    # if params[:artist_id]
+    #   artist = Artist.find_by(id: params[:artist_id])
+    #   if artist.nil?
+    #     redirect_to artists_path, alert: "Artist not found."
+    #   else
+    #     @song = artist.songs.find_by(id: params[:id])
+    #     redirect_to artist_songs_path(artist), alert:  "Song not found." if @song.nil?
+    #   end
+    # else
+    #   @song = Song.find(params[:id])
+    # end  
   end
 
   def update
@@ -65,5 +79,6 @@ class SongsController < ApplicationController
   def song_params
     params.require(:song).permit(:title, :artist_name)
   end
+
 end
 
